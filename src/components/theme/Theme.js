b3585a2db170e1dd09c30e72d6b8525991e69514
Jaimes77 <JaimeRGoff@gmail.com>
2021-09-14 18:10:05 -0400

    fixed theme.js

--- src/components/theme/Theme.js
+++ src/components/theme/Theme.js
@@ -1,15 +1,15 @@
-// import { createTheme } from "@material-ui/core/styles";
-// import red from "@material-ui/core/colors/purple";
-// import orange from "@material-ui/core/colors/green";
+import { createTheme } from "@material-ui/core/styles";
+import red from "@material-ui/core/colors/purple";
+import orange from "@material-ui/core/colors/green";
 
-// const theme = createTheme({
-//   palette: {
-//     primary: {
-//       main: red[500],
-//     },
-//     secondary: {
-//       main: orange[500],
-//     },
-//   },
-// });
-// export default theme;
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: red[500],
+    },
+    secondary: {
+      main: orange[500],
+    },
+  },
+});
+export default theme;
